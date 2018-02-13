fff54e4492bcfb73c4b051b03a03c36b2cbbcf16
Carlton Caven <carlcaven@gmail.com>
2018-02-13 12:04:29 -0800

    local server configured and running

--- server/index.js
+++ server/index.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 
+// app.set('port', 3000);
+app.use(express.static('../client/dist'));
 app.use(bodyParser.json());
-// app.use(express.static(path.join(__dirname, '../client/dist')));
-app.use(express.static(path.join(__dirname, 'server.js')));
+// app.use(express.static(path.join(__dirname, 'server')));
 
-app.listen(3000, function () { console.log('MovieList app listening on port 3000!') });
-
+app.listen(3001, function () { console.log('CSVReportGen app listening on port 3000!') });
